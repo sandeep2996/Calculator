9af53c3c6db2277f8ae017cfa688e7eaa0fa0111
Sandeep <sandeepsoni2996.ss@gmail>
2019-11-06 22:26:47 +0530

    Second Commit

--- src/org/amdocs/Main.java
+++ src/org/amdocs/Main.java
@@ -3,7 +3,6 @@ package org.amdocs;
 public class Main {
     public static void main(String[] args) {
         action a=new add();
-        System.out.println(a.act(9,8));
-
+        System.out.println(a.act(3,5.5));
     }
 }
